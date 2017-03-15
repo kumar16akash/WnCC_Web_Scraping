29facb9672ff838a39b33aa6a66205acfbf488dc
DESKTOP-VJ6R336\dell <kurdleak.alok@gmail.com>
2017-03-15 23:57:28 +0530

    Added comments on url_function.py

--- url_functions.py
+++ url_functions.py
@@ -21,20 +21,26 @@ def get_song_tags( url, artist ):
     html_text = urlopen( url ).read()
 
     soup = BeautifulSoup(html_text, "lxml")
-
+    ### We need only the a tags that have href = ../lyrics/artist_name/song_name.
+    ### The below soup.select helps in parsing. It only accepts tags that are beginning with
+    ### ../lyrics/(artist_name)/
     lyrics_tags = soup.select('a[href^="../lyrics/' + str(artist) + '/"]')
 
     return lyrics_tags
 
-
+### Get the link from the song tag
 def get_lyrics_link( song_tag ):
+
+
+    ### eg: - for the tag - "<a href="../lyrics/adele/daydreamer.html" target="_blank">Daydreamer</a>"
+    ### the link will be :-"http://www.azlyrics.com/lyrics/adele/daydreamer.html"
 
     html_link = "http://www.azlyrics.com/"+str(song_tag)[12:str(song_tag).find("target")-2]
 
     return html_link
 
 
-
+###Go through the html file of the link, and count the occurrences of the word
 def get_occurence( lyric_link, word ):
 
     htmlfile = urlopen(lyric_link)
