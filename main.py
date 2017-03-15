ead080f758dee1bb3cc373ee9b7cba8f6d0e528b
DESKTOP-VJ6R336\dell <kurdleak.alok@gmail.com>
2017-03-15 22:55:11 +0530

    Added all the appropriate functions in main. Main's skeletal is done.

--- main.py
+++ main.py
@@ -1,60 +1,47 @@
-
-
-
-
-import urllib2
-import urlparse
-from bs4 import BeautifulSoup
-import string
-import re
-import time
-from threading import Thread
+import text_file
+import url_functions
 from operator import itemgetter
 
 
 
+names = text_file.get_names()
 
-file = open("List.txt")
+word = text_file.get_word()
 
-file_content = file.read()
-
-table =  file_content.splitlines()
-
-names = table[0:table.index('')]
-curse_word = table[len(table)-1]
 
 record = []
 
 
 for name in names:
-    first_letter = name[0]
-    url = "http://www.azlyrics.com/"+str(first_letter)+"/"+str(name)+".html"
 
-    htmltext = urllib2.urlopen(url).read()
+    base_url = url_functions.get_base_url( name )
+
+    tags = url_functions.get_song_tags( base_url )
+
+
     counter = 0
-    soup = BeautifulSoup(htmltext, "lxml")
+
 
     check = 0
-    for tag in soup.select('a[href^="../lyrics/'+str(name)+'/"]'):
+    for tag in tags:
 
-        #print str(tag) + " " + str(counter)
+
         if check == 3:
             break
 
-        htmllink = "http://www.azlyrics.com/"+str(tag)[12:str(tag).find("target")-2]
-        print htmllink
-        htmlfile = urllib2.urlopen(htmllink)
-        htmltext = htmlfile.read()
-        counter += htmltext.count(str(curse_word))
-        print (counter)
-        time.sleep(5)
+        lyric_link = url_functions.get_lyrics_link( tag )
+
+
+        occurence = url_functions.get_occurence( lyric_link )
+
+        counter+=occurence
         check+=1
-        print check
+
     record.append({'name': name, 'number': counter})
 
 
 
-newlist = sorted(record, key=itemgetter('number'), reverse=True)
+newlist = sorted(record, key = itemgetter('number'), reverse = True)
 
 for dictionary in newlist:
     print dictionary['name']+" -- number of times profanity used -- "+str(dictionary['number'])
